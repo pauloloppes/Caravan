5b732eae2c72ab474a82fcda701bc05cc0e45231
Paulo <pauloloppescoelho@gmail.com>
2019-09-23 21:18:47 -0300

    Initial commit on second version

---
+++ app/src/main/java/com/application/caravan/MainActivity.java
@@ -0,0 +1,26 @@
+package com.application.caravan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Setting up button to open Passenger Add screen
+        final AppCompatButton passengerAdd = (AppCompatButton)findViewById(R.id.buttonAddPassenger);
+        passengerAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PassengerAdd.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/application/caravan/PassengerAdd.java
@@ -0,0 +1,101 @@
+package com.application.caravan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.application.utils.MaskEditUtil;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassengerAdd extends AppCompatActivity {
+
+    EditText editPassengerName;
+    EditText editPassengerIdentity;
+    Spinner spinnerPassengerIdType;
+    EditText editPassengerBirth;
+    EditText editPassengerPhone;
+    EditText editPassengerAddress;
+    AppCompatButton buttonPassengerAdd;
+    FirebaseFirestore databasePassengers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_add);
+
+        databasePassengers = FirebaseFirestore.getInstance();
+
+        //Setting up screen elements
+        editPassengerName = (EditText) findViewById(R.id.editPassengerName);
+        editPassengerBirth = (EditText) findViewById(R.id.editPassengerBirth);
+        editPassengerIdentity = (EditText) findViewById(R.id.editPassengerIdentity);
+        spinnerPassengerIdType = (Spinner) findViewById(R.id.spinnerPassengerIdType);
+        editPassengerPhone = (EditText) findViewById(R.id.editPassengerPhone);
+        editPassengerAddress = (EditText) findViewById(R.id.editPassengerAddress);
+        buttonPassengerAdd = (AppCompatButton) findViewById(R.id.buttonPassengerAdd);
+
+        //Setting up Identity type ID spinner
+        String arrayId[] = {"A","B","C"};
+        ArrayAdapter<String> spinnerAdapterId = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,arrayId);
+        spinnerAdapterId.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPassengerIdType.setAdapter(spinnerAdapterId);
+
+        //Setting up Passenger Identity field, adding listener to apply mask
+        editPassengerBirth.addTextChangedListener(MaskEditUtil.insert(MaskEditUtil.FORMAT_DATE, editPassengerBirth));
+
+        //Setting up button listener
+        buttonPassengerAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addPassenger();
+            }
+        });
+
+    }
+
+    private void addPassenger() {
+
+        Map passenger = new HashMap<>();
+        passenger.put("nome", editPassengerName.getText().toString().trim());
+        passenger.put("dataNascimento", editPassengerBirth.getText().toString().trim());
+        passenger.put("identidade", editPassengerIdentity.getText().toString().trim());
+        passenger.put("tipoIdentidade", spinnerPassengerIdType.getSelectedItem().toString());
+        passenger.put("telefone", editPassengerPhone.getText().toString().trim());
+        passenger.put("endereco", editPassengerAddress.getText().toString().trim());
+
+        databasePassengers.collection("passageiros")
+                .add(passenger)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        toastShow("Passageiro adicionado com sucesso");
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        toastShow("Erro: "+e.getMessage());
+                    }
+                });
+
+    }
+
+    public void toastShow (String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
