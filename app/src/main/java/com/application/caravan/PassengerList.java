755bc85a7182995dea71569468cef797399610ec
Paulo <pauloloppescoelho@gmail.com>
2019-09-29 10:40:44 -0300

    Made search mechanism for passenger list

--- app/src/main/java/com/application/caravan/PassengerList.java
+++ app/src/main/java/com/application/caravan/PassengerList.java
@@ -2,10 +2,13 @@ package com.application.caravan;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,8 +31,12 @@ public class PassengerList extends AppCompatActivity {
 
     private FirebaseFirestore databasePassengers;
     private ArrayList<Passenger> listAll;
+    private ArrayList<Passenger> listSearched;
     private ListView listPassengers;
+    private EditText editPassengerNameSearch;
+    private AppCompatButton buttonSearchPassenger;
     private CustomAdapterPassenger adapter;
+    private CustomAdapterPassenger aSearched;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,54 +44,17 @@ public class PassengerList extends AppCompatActivity {
         setContentView(R.layout.activity_passenger_list);
 
         listPassengers = (ListView) findViewById(R.id.listPassengers);
+        editPassengerNameSearch = (EditText) findViewById(R.id.editPassengerNameSearch);
+        buttonSearchPassenger = (AppCompatButton) findViewById(R.id.buttonSearchPassenger);
 
         listAll = new ArrayList<>();
 
         databasePassengers = FirebaseFirestore.getInstance();
-
-        /*databasePassengers.collection("passageiros").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-
-                if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot doc : task.getResult()) {
-                        Passenger p = doc.toObject(Passenger.class);
-                        System.out.println("                    CRIOU PASSAGEIRO "+p.getNome());
-                        p.setId(doc.getId());
-                        listAll.add(p);
-                    }
-
-                } else {
-                    toastShow("Erro ao acessar documentos: "+task.getException());
-                }
-
-            }
-        });*/
-
         databasePassengers.collection("passageiros").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
             public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                 if (!queryDocumentSnapshots.isEmpty()) {
                     listAll.addAll(queryDocumentSnapshots.toObjects(Passenger.class));
-                    System.out.println("                    CRIOU A LISTA ");
-                    //List<String> tes = new ArrayList<String>();
-
-                    /*for (Passenger p : listAll) {
-                        System.out.println("                  PERCORREU LISTA NOME "+p.getNome());
-                        //toastShow("Leu os cara");
-                        tes.add(p.getNome());
-                    }
-
-
-
-
-
-                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                            getApplicationContext(), android.R.layout.simple_list_item_1,
-                            tes
-                    );
-
-                    listPassengers.setAdapter(arrayAdapter);*/
                     adapter = new CustomAdapterPassenger(listAll, getApplicationContext());
                     listPassengers.setAdapter(adapter);
                 }
@@ -96,21 +66,28 @@ public class PassengerList extends AppCompatActivity {
             }
         });
 
-        /*Task<QuerySnapshot> task = databasePassengers.collection("passageiros").get();
-        if (task.isSuccessful()) {
-            for (DocumentSnapshot doc : task.getResult()) {
-                Passenger p = doc.toObject(Passenger.class);
-                System.out.println("                    CRIOU PASSAGEIRO "+p.getNome());
-                p.setId(doc.getId());
-                listAll.add(p);
+        buttonSearchPassenger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchPassenger();
             }
+        });
+    }
+
+    private void searchPassenger() {
+        String name = editPassengerNameSearch.getText().toString();
+        if (name.isEmpty()) {
+            listPassengers.setAdapter(adapter);
         } else {
-            toastShow("Erro ao acessar documentos: "+task.getException());
-        }*/
-
-
-
-
+            listSearched = new ArrayList<>();
+            for (Passenger p : listAll) {
+                if (p.getNome().contains(name)) {
+                    listSearched.add(p);
+                }
+            }
+            aSearched = new CustomAdapterPassenger(listSearched, getApplicationContext());
+            listPassengers.setAdapter(aSearched);
+        }
     }
 
     private void toastShow (String message) {
